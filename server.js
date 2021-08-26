038ed45dc9691532a5bd266ed3594a9a9094983f
viviane <v.buschwallace@yahoo.com>
2021-08-26 11:16:17 +0200

    env set, opened PORT for server to listen to

---
+++ server.js
@@ -0,0 +1,10 @@
+require("dotenv").config()
+const http = require("http");
+const app = require("./app");
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+server.listen(PORT, ()=>{
+    console.log(`Server is listening on PORT ${PORT} >> http://localhost:${PORT}`)
+})
