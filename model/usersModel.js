8635572e72d9723e946e2449d34d29a5c1f7b181
viviane <v.buschwallace@yahoo.com>
2021-08-26 13:47:29 +0200

    required fields in schema

--- model/usersModel.js
+++ model/usersModel.js
@@ -5,23 +5,29 @@ const userDataSchema = new mongoose.Schema({
   userName: {
     type: String,
     trim: true,
+    required: [true, "Please type your username."],
   },
   userPass: {
     type: String,
     trim: true,
+    required: [true, "Please type your userpass."],
   },
   age: {
     type: Number,
+    required: [true, "Please give your age."],
   },
   fbw: {
     type: Number,
+    required: [true, "Please give your fbw number."],
   },
   toolStack: {
     type: Array,
+    required: false,
   },
   email: {
     type: String,
     trim: true,
+    required: [true, "Please type your email address."],
   },
 });
 
